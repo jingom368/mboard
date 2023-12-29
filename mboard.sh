ed47c989d21d6e770299a48bc211104d8dbff5d0
xaviergold <khw8017@gmail.com>
2023-12-29 11:48:20 +0900

    Update mboard from 202312291148

---
+++ mboard.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ $1 == "start" ]
+   then 
+     docker build -t redleon1/apache2_mboard:1.0 ./apache2/
+     docker build -t redleon1/tomcat9_mboard:1.0 ./tomcat9/
+     docker build -t redleon1/mariadb_mboard:1.0 ./mariadb/
+     docker push redleon1/apache2_mboard:1.0
+     docker push redleon1/tomcat9_mboard:1.0
+     docker push redleon1/mariadb_mboard:1.0
+     docker rmi redleon1/apache2_mboard:1.0
+     docker rmi redleon1/tomcat9_mboard:1.0
+     docker rmi redleon1/mariadb_mboard:1.0
+     kubectl apply -f ./yml/mariadb.yml
+     kubectl apply -f ./yml/tomcat9.yml
+     kubectl apply -f ./yml/apache2.yml
+     kubectl apply -f ./yml/ingress.yml
+fi
+
+if [ $1 == "stop" ]
+   then
+     kubectl delete -f ./yml/ingress.yml
+     kubectl delete -f ./yml/apache2.yml
+     kubectl delete -f ./yml/tomcat9.yml
+     kubectl delete -f ./yml/mariadb.yml
+fi
+
